fa3ee95adb3d67c2c7e8071f955937ecf2dbaebf
Dan BENITAH <dan@local.com>
2019-02-20 20:10:24 +0000

    Resetting the Quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,13 +5,23 @@ import QuizEnd from "./QuizEnd";
 let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { quiz_position: 1 };
+  }
+  handleResetClick(){
+    this.setState({ quiz_position: 1 });
+  }
+  showNextQuestion() {
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1 }
+    });
+  }
   render() {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
-        {isQuizEnd ? (
-          <QuizEnd visible={isQuizEnd} />
-        ) : (
+        {isQuizEnd ?  <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> : (
           <QuizQuestion
             quiz_question={ quizData.quiz_questions[this.state.quiz_position - 1]}
             showNextQuestionHandler={this.showNextQuestion.bind(this)}
@@ -19,15 +29,6 @@ class Quiz extends Component {
         )}
       </div>
     );
-  }
-  constructor(props) {
-    super(props);
-    this.state = { quiz_position: 1 };
-  }
-  showNextQuestion() {
-    this.setState((state) => {
-      return { quiz_position: state.quiz_position + 1 }
-    });
   }
 }
 
