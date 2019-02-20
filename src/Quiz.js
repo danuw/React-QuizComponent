3a64f873c5a74e2edfb30ca86252629a71f202af
Dan BENITAH <dan@local.com>
2019-02-20 00:11:02 +0000

    Displaying Quiz Questions and Handling Events

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react'
-import QuizQuestion from './QuizQuestion'
-import QuizEnd from './QuizEnd';
+import React, { Component } from "react";
+import QuizQuestion from "./QuizQuestion";
+import QuizEnd from "./QuizEnd";
 
-let quizData = require('./quiz_data.json')
-
+let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
-    render() {
-      const isQuizEnd = (this.state.quiz_position -1 === quizData.quiz_questions.length);
-      return (
-        <div>
-            {isQuizEnd?<QuizEnd visible={isQuizEnd}/> :
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
-        </div>
-      )
-    }
-    constructor(props) {
-        super(props)
-        this.state = {quiz_position: 1}
-
-    }
+  render() {
+    const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
+    return (
+      <div>
+        {isQuizEnd ? (
+          <QuizEnd visible={isQuizEnd} />
+        ) : (
+          <QuizQuestion
+            quiz_question={ quizData.quiz_questions[this.state.quiz_position - 1]}
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
+          />
+        )}
+      </div>
+    );
   }
+  constructor(props) {
+    super(props);
+    this.state = { quiz_position: 1 };
+  }
+  showNextQuestion() {
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1 }
+    });
+  }
+}
 
-export default Quiz
+export default Quiz;
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react'
-import QuizQuestionButton from './QuizQuestionButton'
+import React, { Component } from "react";
+import QuizQuestionButton from "./QuizQuestionButton";
 
+class QuizQuestion extends Component {
+  render() {
 
-class QuizQuestion extends Component
-{
-    render() {
-        return (
-            <main>
-                <section>
-                    <p>{this.props.quiz_question.instruction_text}</p>
-                </section>
-                <section className="buttons">
-                    <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
-                    </ul>
-                </section>
-            </main>
-        )
-      }
-      constructor(props) {
-          super(props)
-          this.state = {quiz_position: 1}
-  
-      }
+    return (
+      <main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            {this.props.quiz_question.answer_options.map(
+              (answer_option, index) => {
+                return (
+                  <QuizQuestionButton
+                    key={index}
+                    button_text={answer_option}
+                    clickHandler={this.handleClick.bind(this)}
+                    
+                  />
+                );
+              }
+            )}
+          </ul>
+        </section>
+      </main>
+    );
+  }
+  constructor(props) {
+    super(props);
+    this.state = { quiz_position: 1 };
+  }
+  handleClick(buttonText){
+    if (buttonText === this.props.quiz_question.answer)
+    {
+        this.props.showNextQuestionHandler()
+    }
+  }
 }
 
-export default QuizQuestion
+export default QuizQuestion;
